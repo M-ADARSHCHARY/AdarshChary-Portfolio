86d802ac5bdf919928448068befa229093af77dc
M-ADARSHCHARY <adarshchary.05@gmail.com>
2025-06-21 15:33:41 +0530

    removed netflix clone pjct  (security issues)

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import {Link,Eye , ChevronLeft , ChevronRight} from 'lucide-react'
 import WorkoutTracker from '../PROJECTS/WorkoutTracker'
 import HellowChatApp from '../PROJECTS/HellowChatApp'
-import NetflixClone from '../PROJECTS/NetflixClone'
 const Projects = () => {
   const [projectNum,setProjectNum] = React.useState(0);
-  const projects = [ <HellowChatApp/>, <WorkoutTracker/>, <NetflixClone/>];
+  const projects = [ <HellowChatApp/>, <WorkoutTracker/>];
   const handleChevronLeft = () => {
-    setProjectNum(prevNum => (prevNum === 0 ? 2 : prevNum - 1));
+    setProjectNum(prevNum => (prevNum === 0 ? 1 : prevNum - 1));
   };
   const handleChevronRight = () => {
-    setProjectNum(prevNum => (prevNum === 2 ? 0 : prevNum + 1));
+    setProjectNum(prevNum => (prevNum === 1 ? 0 : prevNum + 1));
   };
   return (
     <section id="Projects" className="min-h-screen flex items-center justify-center py-20 font-sans">
