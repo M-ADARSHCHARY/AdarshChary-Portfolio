95bafecf1b81f44c561ffa899f0f426afe51ee62
M-ADARSHCHARY <adarshchary.05@gmail.com>
2025-06-21 14:53:47 +0530

    added pagination to pjcts

--- src/components/PROJECTS/HellowChatApp.jsx
+++ src/components/PROJECTS/HellowChatApp.jsx
@@ -3,7 +3,7 @@ import {Link , Eye,ExternalLink} from 'lucide-react'
 import hellowChatAppImg from '../../assets/hellow-chatapp.png'
 const HellowChatApp = () => {
   return (
-    <div className="relative p-6 rounded-xl border border-blue-900  hover:translate-y-1 hover:border-blue-500/30">
+    <div className="relative p-6 rounded-xl border border-blue-900  hover:translate-y-1 hover:border-blue-500/30 ">
                     <h3 className="font-sans text-gray-350 font-bold mb-0.2 bg-blue-900 inline-block p-2 rounded-md mb-1">HelloW-ChatApp</h3>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 items-center">
                <div className="border-2 p-2 rounded-md border-white/10 relative hover:scale-102 transition-transform duration-300 ease-in-out">
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
-import {Link,Eye} from 'lucide-react'
+import {Link,Eye , ChevronLeft , ChevronRight} from 'lucide-react'
 import WorkoutTracker from '../PROJECTS/WorkoutTracker'
 import HellowChatApp from '../PROJECTS/HellowChatApp'
 import NetflixClone from '../PROJECTS/NetflixClone'
 const Projects = () => {
+  const [projectNum,setProjectNum] = React.useState(0);
+  const projects = [ <HellowChatApp/>, <WorkoutTracker/>, <NetflixClone/>];
+  const handleChevronLeft = () => {
+    setProjectNum(prevNum => (prevNum === 0 ? 2 : prevNum - 1));
+  };
+  const handleChevronRight = () => {
+    setProjectNum(prevNum => (prevNum === 2 ? 0 : prevNum + 1));
+  };
   return (
     <section id="Projects" className="min-h-screen flex items-center justify-center py-20 font-sans">
 
         <div className="max-w-5xl mx-auto px-4">
             <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">Featured Projects:</h2>
-            <div className="grid grid-cols-1 gap-3">
-                 {/* Project-1 */}
-                 <HellowChatApp/> 
-                 {/* Project-2 */}
-                 <WorkoutTracker/> 
-                 {/* Project-3 */}  
-                 <NetflixClone/>
+            <div className="flex items-center animate-fadeIn transition-all duration-500 ease-out" key={projectNum}>
+                <button onClick= {handleChevronLeft} className="hover:text-blue-600" ><ChevronLeft size={28}/> </button>
+                  <div className="grid grid-cols-1 gap-3">
+                    {projects[projectNum]}
+                  </div>
+                <button onClick= {handleChevronRight} className="hover:text-blue-600"><ChevronRight size={28} /></button>
             </div>
         </div>
     </section>
